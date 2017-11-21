1b3a9aa9187f3feb87d2c42a1bfec4b25a21f6b8
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-11-21 15:40:40 +0300

    capture input from user and emmit it to parent component user emiiter

--- src/app/new-food/new-food.component.ts
+++ src/app/new-food/new-food.component.ts
@@ -9,6 +9,10 @@ import { Component, Output, EventEmitter} from '@angular/core';
 })
 export class NewFoodComponent implements OnInit {
   @Output() newFoodSender=new EventEmitter();
+  addFood(name:string,description:string,calories:number){
+  var newFoodToAdd:Food= new Food(name,description,calories);
+  this.newFoodSender.emit(newFoodToAdd);
+}
 
 
   constructor() { }
